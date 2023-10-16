b9b961e49b7e5a527ad5a59a4629d100df4f03b7
Bequiman <thales.medeiros@estudante.iftm.edu.br>
2023-10-16 19:09:07 -0300

    atualizando

---
+++ projeto-ana/src/main/java/model/Matricula.java
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+package model;
+
+public class Matricula {
+	private int  id;
+	private int  ano;
+	private int  semestre;
+	private int  fkaluno;
+	private int  fkdisciplina; 
+}
+=======
+package model;
+
+public class Matricula {
+	private int  id;
+	private int  ano;
+	private int  semestre;
+	private int  fkaluno;
+	private int  fkdisciplina;
+}
+>>>>>>> branch 'main' of https://github.com/anaclara1925/projesof.git
