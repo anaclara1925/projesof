7313a7a2ff50e5fc83e41978a273778bba7c13de
Bequiman <thales.medeiros@estudante.iftm.edu.br>
2023-10-16 20:34:26 -0300

    Atividade A3

--- projeto-ana/src/main/java/model/Disciplina.java
+++ projeto-ana/src/main/java/model/Disciplina.java
@@ -1,7 +1,19 @@
 
 package model;
 
-public class Disciplina {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Disciplina implements Serializable{	
+	private static final long serialVersionUID = 1L;
+	
+	@Id 
+	@GeneratedValue(strategy=GenerationType.IDENTITY) 
 	private Integer  id;
 	private Integer  carga_horaria;
 	private String nome;
--- projeto-ana/src/main/java/model/Matricula.java
+++ projeto-ana/src/main/java/model/Matricula.java
@@ -1,10 +1,77 @@
 
 package model;
 
-public class Matricula {
-	private int  id;
-	private int  ano;
-	private int  semestre;
-	private int  fkaluno;
-	private int  fkdisciplina;
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Matricula implements Serializable{	
+	private static final long serialVersionUID = 1L;
+	@Id 
+	@GeneratedValue(strategy=GenerationType.IDENTITY) 
+	private Integer  id;
+	private Integer  ano;
+	private Integer  semestre;
+	private Integer  fkaluno;
+	private Integer  fkdisciplina;
+	
+	public Matricula(Integer id, Integer ano, Integer semestre, Integer fkaluno, Integer fkdisciplina) {
+		super();
+		this.id = id;
+		this.ano = ano;
+		this.semestre = semestre;
+		this.fkaluno = fkaluno;
+		this.fkdisciplina = fkdisciplina;
+	}
+
+	public Matricula() {
+		super();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getAno() {
+		return ano;
+	}
+
+	public void setAno(Integer ano) {
+		this.ano = ano;
+	}
+
+	public Integer getSemestre() {
+		return semestre;
+	}
+
+	public void setSemestre(Integer semestre) {
+		this.semestre = semestre;
+	}
+
+	public Integer getFkaluno() {
+		return fkaluno;
+	}
+
+	public void setFkaluno(Integer fkaluno) {
+		this.fkaluno = fkaluno;
+	}
+
+	public Integer getFkdisciplina() {
+		return fkdisciplina;
+	}
+
+	public void setFkdisciplina(Integer fkdisciplina) {
+		this.fkdisciplina = fkdisciplina;
+	}
+	
+	
+	
 }
