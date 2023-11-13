af4599827d81ecd54fb0f7451e3fa1d53d11ff6b
Bequiman <thales.medeiros@estudante.iftm.edu.br>
2023-11-13 20:30:31 -0300

    Fazendo ainda A4

--- projeto-ana/src/main/java/control/DisciplinaControl.java
+++ projeto-ana/src/main/java/control/DisciplinaControl.java
@@ -1,14 +1,15 @@
 package control;
+
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.EntityManager;
 
 public class DisciplinaControl {
 	private EntityManager em;
-	
-	public DisciplinaControl()
-	EntityManagerFactory emf = Persistence.createEntityManagerFactory("projeto");
-	em = emf.createEntityManager();
-}
+
+	public DisciplinaControl() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("projeto");
+		em = emf.createEntityManager();
+	}
 
 }
